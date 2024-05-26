ad769e0b66451e84815eca6010b06417c689e142
xuxihai <xuxihai123@gmail.com>
2024-05-26 16:06:02 +0800

    add question

--- testgearapi/uploadapp.js
+++ testgearapi/uploadapp.js
@@ -23,7 +23,7 @@ const program = {
 
 try {
   const meta = ProgramMetadata.from("0x" + fs.readFileSync(resolve("tmg.meta.txt")));
-  const { programId, salt, submitted } = await gearApi.program.upload(program, meta);
+  const { programId, salt, submitted, extrinsic } = await gearApi.program.upload(program, meta);
   console.log("programId====", programId);
   console.log("salt====", salt);
   console.log("submitted====", submitted);
@@ -39,3 +39,8 @@ try {
 } catch (error) {
   console.error(`${error.name}: ${error.message}`);
 }
+
+
+// 1. 名字是一串hex的问题  //见  https://idea.gear-tech.io/programs/0x4a32a284c5b52f15d1ecbf0a1dd9f20ff0ae366ab36630f83dfbd5edd294bcc6
+// 2. 状态不正常的问题     //见 https://idea.gear-tech.io/programs/0x4a32a284c5b52f15d1ecbf0a1dd9f20ff0ae366ab36630f83dfbd5edd294bcc6
+// 3. initPayload和meta关系，上传时如何处理initPayload?
